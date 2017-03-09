8ce17ba0e85d4d8fe06b2b3cb7a226beaa7d9e5e
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-03-09 12:03:37 +0100

    catch file instantiation errors (for multi-line scripts etc.)
    avoids "Could not normalize path for file" error on Windows

--- src/main/groovy/PraatExec.groovy
+++ src/main/groovy/PraatExec.groovy
@@ -11,8 +11,13 @@ class PraatExec extends DefaultTask {
 
     @TaskAction
     void exec() {
-        def scriptFile = project.file(script)
-        if (!scriptFile.canRead() && !scriptFile.isFile()) {
+        def scriptFile
+        try {
+            scriptFile = project.file(script)
+        } catch (all) {
+            project.logger.error "Failed to instantiate scriptFile"
+        }
+        if (!scriptFile || !scriptFile.canRead() && !scriptFile.isFile()) {
             scriptFile = project.file("$temporaryDir/script.praat")
             scriptFile.text = script
         }
